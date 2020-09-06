50e554402bb7f8ccdb24d2e137827f6158a6d5e9
cybernetist <aydinmeydanioglu@yandex.com>
2020-09-06 12:59:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Nav from './components/Nav'
+
+export default function App() {
+  return (
+    <div>
+      <Nav/>
+    </div>
+  );
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navbar } from 'react-bootstrap';
+
+
+const Nav = () => {
+    return (
+        <Navbar bg="light" expand="lg" >
+
+                <Navbar.Brand href="/">BASİC CRUD APP</Navbar.Brand>
+
+        </Navbar>
+    )
+}
+
+
+export default Nav;
